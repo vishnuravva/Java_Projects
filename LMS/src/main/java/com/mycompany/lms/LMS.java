ade3c54616fba2fd3842a36ad4158325306abf7d
vishnuravva <ravvavishnu68@gmail.com>
2024-06-11 00:18:15 +0530

    initial commit

---
+++ LMS/src/main/java/com/mycompany/lms/LMS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lms;
+
+/**
+ *
+ * @author ravva
+ */
+public class LMS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
